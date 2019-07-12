638c280ffdf8ed4e1f3905d9e0fb0ab83419de9e
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-07-12 15:51:01 +0300

    remove ColorMode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { ThemeProvider, Styled, ColorMode } from 'theme-ui'
+import { ThemeProvider, Styled } from 'theme-ui'
 import theme from './components/theme'
 import './lib/reset.css'
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
-    <ColorMode />
     <Styled.root>{element}</Styled.root>
   </ThemeProvider>
 )
