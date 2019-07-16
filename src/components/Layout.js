ef2910cc62d650e8f4a6ac845d33dbac030b1ee6
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-07-16 09:07:29 +0300

    export default

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,25 +4,21 @@ import { graphql } from 'gatsby'
 import Link from 'components/link'
 import eggo from '../../assets/eggo.svg'
 
-function pageLayout({ frontmatter = {}, children }) {
-  return (
-    <Layout>
-      <Header>
-        <Container>
-          <Link to="/">
-            <img src={eggo} alt="" />
-          </Link>
-        </Container>
-      </Header>
-      <Main>{children}</Main>
-      <Footer>
-        <Container>@eggheadio</Container>
-      </Footer>
-    </Layout>
-  )
-}
-
-export default pageLayout
+export default ({ frontmatter = {}, children }) => (
+  <Layout>
+    <Header>
+      <Container>
+        <Link to="/">
+          <img src={eggo} alt="" />
+        </Link>
+      </Container>
+    </Header>
+    <Main>{children}</Main>
+    <Footer>
+      <Container>@eggheadio</Container>
+    </Footer>
+  </Layout>
+)
 
 export const pageQuery = graphql`
   fragment site on Site {
