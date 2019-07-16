c519f0ffb964957fbe4e699a48c19366421275d9
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-07-16 15:21:57 +0300

    turn link.js back

---
+++ src/components/link.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import GatsbyLink from 'gatsby-link'
+
+const Link = ({ children, to, ...restProps }) => {
+  const internal = /^\/(?!\/)/.test(to)
+
+  if (internal) {
+    return (
+      <GatsbyLink to={to} {...restProps}>
+        {children}
+      </GatsbyLink>
+    )
+  }
+
+  return (
+    <a href={to} {...restProps}>
+      {children}
+    </a>
+  )
+}
+
+export default Link
