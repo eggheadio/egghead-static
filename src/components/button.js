1d35f1b49999f868f32f1e97a4191fb6a70256c3
evgeniy.nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-07-17 19:11:39 +0300

    add PropTypes

--- src/components/button.js
+++ src/components/button.js
@@ -1,23 +1,36 @@
 /** @jsx jsx */
+import PropTypes from 'prop-types'
 import { jsx } from 'theme-ui'
 import Link from 'components/link'
 
-const Button = ({
-  to,
-  children,
-  kind = 'default',
-  type = 'button',
-  ...restProps
-}) => {
+const Button = ({ kind, to, type, sx, children, ...restProps }) => {
   return to ? (
-    <Link to={to} sx={{ variant: `buttons.${kind}` }} {...restProps}>
+    <Link to={to} sx={{ variant: `buttons.${kind}`, ...sx }} {...restProps}>
       {children}
     </Link>
   ) : (
-    <button type={type} sx={{ variant: `buttons.${kind}` }} {...restProps}>
+    <button
+      type={type}
+      sx={{ variant: `buttons.${kind}`, ...sx }}
+      {...restProps}
+    >
       {children}
     </button>
   )
 }
+
+Button.propTypes = {
+  kind: PropTypes.oneOf(['default', 'primary']),
+  to: PropTypes.string,
+  type: PropTypes.string,
+  sx: PropTypes.object,
+}
+
+Button.defaultProps = {
+  kind: 'default',
+  type: 'button',
+}
+
+Button.displayName = 'Button'
 
 export default Button
