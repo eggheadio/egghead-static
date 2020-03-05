ca3c51e02394acdc4eb67dc7b5ecb745ee6248d8
Vojta Holik <vojta@egghead.io>
2020-03-05 13:12:01 +0100

    move images under /static/learn to make them work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import { Styled } from 'theme-ui'
-
 import Layout from '../components/layout'
-import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout title="Learn">
