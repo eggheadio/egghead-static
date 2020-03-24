cfb7343df55524a149403eafa4efe598b1efb68c
Vojta Holik <vojta@egghead.io>
2020-03-24 17:29:37 +0100

    tweaks

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx, Styled } from 'theme-ui'
 import Layout from './layout'
+import { isEmpty } from 'lodash'
 
 export default function Post({
   children,
@@ -11,56 +12,47 @@ export default function Post({
 }) {
   return (
     <Layout title={title} description={excerpt} maxWidth={1040} {...props}>
-      <Styled.h2 sx={{ mt: 3 }}>Hi there,</Styled.h2>
-      <Styled.p>
-        Here's an update on what's been going on around egghead over the last
-        week.
-      </Styled.p>
-      {children}
-      <Styled.p sx={{ mt: 4, fontStyle: 'italic' }}>
-        Cheers,
-        <br />
-        your friends at{' '}
-        <Styled.a href="https://egghead.io">egghead.io</Styled.a>
-      </Styled.p>
-      <ul
-        sx={{
-          borderTop: '1px solid #f1f1f1',
-          py: 3,
-          mt: 4,
-          fontSize: 1,
-          textTransform: 'capitalize',
-          display: 'flex',
-          flexWrap: 'wrap',
-          listStyleType: 'none',
-          ml: '0 !important',
-          pl: 0,
-          li: {
-            ml: 0,
-          },
-        }}
-      >
-        <li
+      <div sx={{ mt: 3 }}>{children}</div>
+      {!isEmpty(tags) && !isEmpty(keywords) && (
+        <ul
           sx={{
-            fontWeight: 500,
-            textTransform: 'uppercase',
-            mr: 3,
-            opacity: 0.8,
+            borderTop: '1px solid #f1f1f1',
+            py: 3,
+            mt: 4,
+            fontSize: 1,
+            textTransform: 'capitalize',
+            display: 'flex',
+            flexWrap: 'wrap',
+            listStyleType: 'none',
+            ml: '0 !important',
+            pl: 0,
+            li: {
+              ml: 0,
+            },
           }}
         >
-          week {date}
-        </li>
-        {tags.map(tag => (
-          <li sx={{ opacity: 0.7, mr: 2 }} key={tag}>
-            {tag}
+          <li
+            sx={{
+              fontWeight: 500,
+              textTransform: 'uppercase',
+              mr: 3,
+              opacity: 0.8,
+            }}
+          >
+            week {date}
           </li>
-        ))}
-        {keywords.map(keyword => (
-          <li sx={{ opacity: 0.7, mr: 2 }} key={keyword}>
-            {keyword}
-          </li>
-        ))}
-      </ul>
+          {tags.map(tag => (
+            <li sx={{ opacity: 0.7, mr: 2 }} key={tag}>
+              {tag}
+            </li>
+          ))}
+          {keywords.map(keyword => (
+            <li sx={{ opacity: 0.7, mr: 2 }} key={keyword}>
+              {keyword}
+            </li>
+          ))}
+        </ul>
+      )}
     </Layout>
   )
 }
